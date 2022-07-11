9058fdeec1bae39ffb27239a205a08b86b3a02e5
hahbr88 <90291796+hahbr88@users.noreply.github.com>
2022-07-12 02:23:04 +0900

    Flask 서버 구동을 위한 app.py 작성, review.html에 예시 코드 첨부, Jinja2 활용을 위한 dir 추가 #1

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+app = Flask(__name__)
+
+# from pymongo import MongoClient
+# client = MongoClient('mongodb+srv://frago:G8JQhmTgex80D5NV@cluster0.3pkyv7h.mongodb.net/Cluster0?retryWrites=true&w=majority')
+# db = client.dbRanunculus
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/review')
+def showReview():
+    return render_template('review.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
