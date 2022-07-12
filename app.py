f3fb38c54f700ae10df45b92de6695730a52a91c
bigbit99 <bigbit831@gmail.com>
2022-07-12 20:02:55 +0900

    회원가입 클라이언트 추가

--- app.py
+++ app.py
@@ -52,8 +52,9 @@ def sign_up():
 
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
-    # ID 중복확인
-    return jsonify({'result': 'success'})
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 
 if __name__ == '__main__':
