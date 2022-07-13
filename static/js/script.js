998d5027567c500c1dc7931a0384a4fc473a50b9
hahbr88 <90291796+hahbr88@users.noreply.github.com>
2022-07-13 09:33:53 +0900

    index.html , review.html 수정, 리뷰페이지 로딩시 필요한 함수 수정

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,4 @@
-$(document).ready(function () {
-    show_review();
-});
+////////// 리뷰 페이지 JS //////////
 
 function show_review() {
     $('.card-content').empty()
@@ -56,14 +54,70 @@ function save_review() {
     }
 }
 
-
-
-
-// function toggle_review_form() {
+// function if_not_member() {
 //     $("#review_form").toggleClass("is-hidden")
 // }
 
+////////// 메인 페이지 JS //////////
 
-function if_not_member() {
-    $("#review_form").toggleClass("is-hidden")
+// https://openweathermap.org/current
+// https://openweathermap.org/weather-conditions
+const API_KEY = "9eb9bef9a874a362a38c4362cb02b64e";
+
+function onGeoSucess(position) {
+    console.log(position)
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log(lat)
+    console.log(lon)
+
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=kr&appid=${API_KEY}`;
+    fetch(API_URL)
+        .then((response) => response.json())
+        .then((data) => {
+            const WIcon = document.querySelector("#weather span:first-child");
+            const WTalk = document.querySelector("#weather span:last-child");
+            const WMain = data.weather[0].main
+            const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+            WIcon.innerHTML = `<img src="${iconUrl}">`;
+            if (WMain === "Rain") {
+                WTalk.innerText = "비오는 날 문구"
+            } else if (WMain === "Clear") {
+                WTalk.innerText = '화창한 날 문구';
+            } else if (WMain === "Claud") {
+                WTalk.innerText = '흐린 날 문구';
+            } else if (WMain === "Thunderstorm") {
+                WTalk.innerText = '천둥 번개 문구';
+            } else if (WMain === "Drizzle") {
+                WTalk.innerText = '이슬비 문구';
+            } else if (WMain === "Snow") {
+                WTalk.innerText = '눈오는 날 문구';
+            } else {
+                WTalk.innerText = "그냥 문구"
+            }
+            console.log(data.weather[0].main);
+        })
 }
+
+function onGeoError() {
+    alert("사용자의 위치를 찾을 수 없음");
+}
+
+function getWeather(data) {
+    const WIcon = document.querySelector("#weather span:first-child");
+    const WTalk = document.querySelector("#weather span:last-child");
+    const WMain = data.weather[0].main
+    const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    WIcon.innerHTML = `<img src="${iconUrl}">`;
+    if (WMain === "rain") {
+        WTalk.innerText = "비오는 날 문구"
+    } else if (WMain === "") {
+        WTalk.innerText = '';
+    } else {
+        WTalk.innerText = "그냥 문구"
+    }
+    console.log(data.weather[0].main);
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSucess, onGeoError);
+// https://developer.mozilla.org/ko/docs/Web/API/Geolocation/getCurrentPosition
