7087c2a8c5d14db8f2cb12d52d0dda55f2889c25
sonana <4775614@naver.com>
2022-07-14 00:00:23 +0900

    Jquery 확인 #3

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 
-
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
@@ -35,7 +34,6 @@ def login():
     return render_template('login.html', msg=msg)
 
 
-
 @app.route('/review')
 def review():
     return render_template('review.html')
@@ -48,13 +46,13 @@ def sign_up():
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     number_receive = request.form['number_give']
     doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "number": number_receive,                                   # 전화번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-        "profile_pic": "",                                          # 프로필 사진 파일 이름
-        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        "profile_info": ""                                          # 프로필 한 마디
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "number": number_receive,  # 전화번호
+        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
+        "profile_pic": "",  # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
+        "profile_info": ""  # 프로필 한 마디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -87,11 +85,11 @@ def get_review():
     return jsonify({'reviews': review_list})
 
 
-
 # reservation
 @app.route('/reservation')
 def showreservation():
     return render_template('reservation.html')
+
 
 @app.route("/reservation", methods=["POST"])
 def web_reservation_post():
@@ -99,6 +97,46 @@ def web_reservation_post():
     date_receive = request.form['date_give']
     time_receive = request.form['time_give']
     comment_receive = request.form['comment_give']
+
+    db.wow.find({})
+
+    a = db.ranunculus.find({"date": date_receive}, {"time": time_receive})
+    print(len(list(a)))
+
+
+#     if len list 0 이면
+#         들어가라. (중복이 없다는 뜻이니까;;)
+#     1부터면 중복이
+#     print(잇다는뜻이다)
+#      128번쨰를 안하면 댐.
+#
+#     if length 0 이면 128을 하고
+#         아니면(1이상이면) 안해.
+#
+#
+# if len list 0 이면
+# db.ranunculus.insert_one(doc)
+#
+#     return jsonify({'msg': '예약 완료되었습니다.'})
+#
+#     else {
+# return jsonify({'msg': '중복이삼.'})
+# }
+
+
+
+    # a = db.ranunculus.find({"date": date_receive})
+
+    # print(db.ranunculus.find({"date" : date_receive})
+
+    # print(a.scalar())
+    # print(a.pretty())
+
+
+    # print(a.시간)
+
+
+
     doc = {
         'name': name_receive,
         'date': date_receive,
@@ -109,6 +147,7 @@ def web_reservation_post():
 
     return jsonify({'msg': '예약 완료되었습니다.'})
 
+
 #
 # @app.route("/reservation", methods=["GET"])
 # def web_reservation_get():
@@ -118,8 +157,7 @@ def web_reservation_post():
 @app.route("/rev", methods=["GET"])
 def web_reservation_get():
     reservation_list = list(db.ranunculus.find({}, {'_id': False}))
-    return jsonify({'reservations':reservation_list})
-
+    return jsonify({'reservations': reservation_list})
 
 
 if __name__ == '__main__':
